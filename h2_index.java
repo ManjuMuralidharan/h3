417fdd501217c6f69e23a5052659c625c0618f67
ManjuMuralidharan <manju1695@gmail.com>
2016-08-30 14:49:17 +0530

    Add files via upload

---
+++ h2_index.java
@@ -0,0 +1,20 @@
+package h2_index;
+
+public class h2_index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] a={-3,-1,1,3,5};
+		
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==i)
+			{
+				System.out.println("Index of - "+a[i]+"-->"+i);
+			}
+		}
+
+	}
+
+}
